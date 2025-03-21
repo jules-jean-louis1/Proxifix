dc94f82cd2bba17da06124ef0136c7ea5f5a2717
Axel Vair <Axel>
2025-03-21 10:38:42 +0100

    Style: ended tabbar style

--- mobile/app/navigation/TabBar.jsx
+++ mobile/app/navigation/TabBar.jsx
@@ -1,17 +1,30 @@
-import { View, StyleSheet } from 'react-native';
-import { useLinkBuilder, useTheme } from '@react-navigation/native';
-import { Text, PlatformPressable } from '@react-navigation/elements';
-import {colors} from "@/styles/globalStyles";
+import {View, StyleSheet} from 'react-native';
+import {useLinkBuilder, useTheme} from '@react-navigation/native';
+import {Text, PlatformPressable} from '@react-navigation/elements';
+import {Feather} from "@expo/vector-icons";
 
-export function TabBar({ state, descriptors, navigation }) {
-    const { colors } = useTheme();
-    const { buildHref } = useLinkBuilder();
+export function TabBar({state, descriptors, navigation}) {
+    const {colors} = useTheme();
+    const {buildHref} = useLinkBuilder();
+
+    // Handle icons with route name
+    const icon = {
+        index: (props) => <Feather name="home" size={24} {...props} />,
+        interventions: (props) => <Feather name="tool" size={24} {...props} />,
+        profile: (props) => <Feather name="settings" size={24} {...props} />,
+    }
+
+    // stock current route name
     const currentRoute = state.routes[state.index].name;
+
+    // we don't want tabbar on index/home page
     if (currentRoute === 'index') return null;
+
     return (
         <View style={styles.tabbar}>
             {state.routes.map((route, index) => {
-                const { options } = descriptors[route.key];
+                const {options} = descriptors[route.key];
+
                 const label =
                     options.tabBarLabel !== undefined
                         ? options.tabBarLabel
@@ -44,14 +57,24 @@ export function TabBar({ state, descriptors, navigation }) {
                     <PlatformPressable
                         key={route.key}
                         href={buildHref(route.name, route.params)}
-                        accessibilityState={isFocused ? { selected: true } : {}}
+                        accessibilityState={isFocused ? {selected: true} : {}}
                         accessibilityLabel={options.tabBarAccessibilityLabel}
-                        testID={options.tabBarButtonTestID}
                         onPress={onPress}
                         onLongPress={onLongPress}
-                        style={ styles.tabbarItem }
-                    >
-                        <Text style={{ color: isFocused ? colors.primary : colors.text }}>
+                        style={[styles.tabbarItem, isFocused]
+                    }>
+                        <View style={[
+                            styles.iconContainer,
+                            isFocused && styles.focusedIconContainer
+                        ]}>
+                            {icon[route.name]({
+                                color: isFocused,
+                            })}
+                        </View>
+                        <Text style={[
+                            { color: '#000' },
+                            styles.label
+                        ]}>
                             {label}
                         </Text>
                     </PlatformPressable>
@@ -68,13 +91,28 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-around',
-        backgroundColor: '#fff'
+        backgroundColor: '#fff',
+        borderColor: '#00000033',
+        borderTopWidth: 1,
     },
     tabbarItem: {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: '#fff',
-        gap: 5
+        gap: 2,
+
+    },
+    iconContainer: {
+        paddingVertical: 8,
+        paddingHorizontal: 25,
+        borderRadius: 20,
+    },
+    focusedIconContainer: {
+        backgroundColor: '#FFD1DC',
+    },
+    label: {
+        fontWeight: 'bold',
+        marginBottom: 8,
     }
 })
