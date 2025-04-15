2f9e0e4ce34038774a21ab4d4dc3a83106a0c8d0
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2025-04-15 18:58:49 +0200

    feat: add layouts and registration form for admin and customer sections

---
+++ mobile/app/(tabs)/_layout.tsx
@@ -0,0 +1,31 @@
+import { Stack, useRouter } from "expo-router";
+import { useSession } from "../context/ctx";
+import { useEffect } from "react";
+import { ToolBarCustomer } from "../components/navigations/ToolBarCustomer";
+
+function ProtectedLayout({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+  const { session, isLoading } = useSession();
+
+  useEffect(() => {
+    if (!isLoading && !session) {
+      router.replace("../loginCustomer");
+    }
+  }, [isLoading, session]);
+
+  if (isLoading) {
+    // Afficher un écran de chargement pendant que la session est vérifiée
+    return null;
+  }
+
+  return <>{children}</>;
+}
+
+export default function MainLayout() {
+  return (
+    <ProtectedLayout>
+      <ToolBarCustomer />
+      <Stack screenOptions={{headerShown : false}} />
+    </ProtectedLayout>
+  );
+}
