4d61bd019be8591f2cc12ff235e964c741a81c37
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2025-04-15 08:55:23 +0200

    refactor: enhance ToolBarCustomer component with improved layout and icon button

--- mobile/app/components/navigations/ToolBarCustomer.tsx
+++ mobile/app/components/navigations/ToolBarCustomer.tsx
@@ -1,23 +1,45 @@
 import { useSession } from "@/app/context/ctx";
-import { View, StyleSheet } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { View, StyleSheet, Pressable } from "react-native";
 import { IconButton } from "react-native-paper";
 
 export const ToolBarCustomer = () => {
-    const { signOut } = useSession();
+  const { signOut } = useSession();
   return (
     <View style={style.container}>
-      <IconButton
-        icon="account"
-        size={20}
-        onPress={() => signOut()}
-      />
+      <View style={style.subContainer}>
+        <Pressable
+          onPress={() => signOut()}
+          style={{
+            backgroundColor: "#F5F5F8",
+            padding: 10,
+            borderRadius: 50,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Feather
+            name="user"
+            size={24}
+            color="black"
+          />
+        </Pressable>
+      </View>
     </View>
   );
 };
 
 const style = StyleSheet.create({
-    container : {
-        flexDirection: "row",
-        justifyContent: "flex-end"
-    }
-})
+  container: {
+    width: "100%",
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  subContainer: {
+    paddingVertical: 5,
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    width: "90%",
+  },
+});
